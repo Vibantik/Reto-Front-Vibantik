bb80a0c193c74ed04c322e3f574177fbea6099fc
mar-hz <103940408+mar-hz@users.noreply.github.com>
2026-04-13 23:08:32 -0600

    Agregar menu sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import { Save } from 'lucide-react';
+import './css/Sidebar.css';
+import '../App.css';
+
+const MOCK_CONFIG_DATA = [
+    { id: 1, label: 'Categorización Automática', defaultState: true },
+    { id: 2, label: 'Módulo Presupuestos', defaultState: true },
+    { id: 3, label: 'Módulo Ahorros', defaultState: true },
+    { id: 4, label: 'Mostrar Ahorros Terminados', defaultState: true },
+    { id: 5, label: 'Reportes Inteligentes', defaultState: true },
+    { id: 6, label: 'Asesor IA', defaultState: true },
+    { id: 7, label: 'Mensajes de Asesor IA', defaultState: true },
+];
+
+const settingsData = MOCK_CONFIG_DATA;
+
+export default function Sidebar({ isOpen }) {
+    const [toggles, setToggles] = useState(() => {
+        const initialState = {};
+        settingsData.forEach((item) => {
+            initialState[item.id] = item.defaultState;
+        });
+        return initialState;
+    });
+
+    const handleToggle = (key) => {
+        setToggles((prev) => ({ ...prev, [key]: !prev[key] }));
+    };
+
+    return (
+        <>
+            <div className={`sidebar ${isOpen ? 'open' : ''}`}>
+                <div className="sidebar-header">
+                    <h3>Configuración</h3>
+                </div>
+
+                <ul className="sidebar-list">
+                    {settingsData.map((item) => (
+                        <li
+                            key={item.id}
+                            className="sidebar-item"
+                            onClick={() => handleToggle(item.id)}
+                        >
+                            <span className="sidebar-label">{item.label}</span>
+                            <div className={`toggle-switch ${toggles[item.id] ? 'active' : ''}`}>
+                                <div className="toggle-slider"></div>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+                <button className="btn-action" style={{ marginLeft: '20px', marginTop: '10px', display: 'flex', alignItems: 'center', gap: '5px' }}><Save /> Guardar</button>
+            </div>
+        </>
+    );
+}
