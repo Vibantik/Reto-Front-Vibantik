e80a630ad918a3ed7babce60d3fabbdb7179aed5
Diego García González <Diego.Garcia.Gzz@outlook.com>
2026-04-27 10:49:35 -0600

    ajuste test

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -21,8 +21,8 @@ beforeEach(() => {
     req.reply({ statusCode: 200, headers: { 'Content-Type': 'text/event-stream' }, body: '' })
   })
 
-  cy.intercept('GET', /\/api\/transactions\?.*type=ingreso.*/, { fixture: 'transactions-ingreso.json' })
-  cy.intercept('GET', /\/api\/transactions\?.*type=egreso.*/, { fixture: 'transactions-egreso.json' })
-  cy.intercept('GET', /\/api\/transactions\?.*category=Comida.*/, { fixture: 'transactions-comida.json' })
-  cy.intercept('GET', /\/api\/transactions/, { fixture: 'transactions.json' })
+  cy.intercept('GET', /\/api\/transactions\?.*type=ingreso/, { fixture: 'transactions-ingreso.json' })
+  cy.intercept('GET', /\/api\/transactions\?.*type=egreso/, { fixture: 'transactions-egreso.json' })
+  cy.intercept('GET', /\/api\/transactions\?.*category=Comida/, { fixture: 'transactions-comida.json' })
+  cy.intercept('GET', /\/api\/transactions(\?(?!.*type=ingreso)(?!.*type=egreso)(?!.*category=Comida).*)?$/, { fixture: 'transactions.json' })
 })
