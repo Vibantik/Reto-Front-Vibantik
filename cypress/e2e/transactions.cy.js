1b8719d51cdb0b64a981f73adae731324f0d41f0
Victor Abel Camacho Rodríguez <A01233745@tec.mx>
2026-05-03 23:08:00 -0600

    si

--- cypress/e2e/transactions.cy.js
+++ cypress/e2e/transactions.cy.js
@@ -22,42 +22,39 @@ describe('Transacciones E2E', () => {
   })
 
   it('filtra por tipo ingreso', () => {
-    cy.get('[data-cy="transaction-type-filter"]')
-      .select('ingreso')
-      .should('have.value', 'ingreso')
-
+    cy.get('[data-cy="transaction-type-filter"]').select('ingreso').should('have.value', 'ingreso')
+    cy.get('[data-cy="transaction-card"]').should('have.length.greaterThan', 0)
     cy.get('[data-cy="transaction-card"]').each(($card) => {
       cy.wrap($card).should('contain.text', 'Ingreso')
     })
   })
 
   it('filtra por tipo egreso', () => {
-    cy.get('[data-cy="transaction-type-filter"]')
-      .select('egreso')
-      .should('have.value', 'egreso')
-
+    cy.get('[data-cy="transaction-type-filter"]').select('egreso').should('have.value', 'egreso')
+    cy.get('[data-cy="transaction-card"]').should('have.length.greaterThan', 0)
     cy.get('[data-cy="transaction-card"]').each(($card) => {
       cy.wrap($card).should('contain.text', 'Egreso')
     })
   })
 
   it('filtra por categoría comida', () => {
-    cy.get('[data-cy="transaction-category-filter"]')
-      .select('Comida')
-      .should('have.value', 'Comida')
-
+    cy.get('[data-cy="transaction-category-filter"]').select('Comida').should('have.value', 'Comida')
+    cy.get('[data-cy="transaction-card"]').should('have.length.greaterThan', 0)
     cy.get('[data-cy="transaction-card"]').each(($card) => {
       cy.wrap($card).should('contain.text', 'Comida')
     })
   })
 
   it('permite capturar rango de fechas', () => {
-    cy.get('[data-cy="transaction-start-date"]')
-      .type('2025-01-01')
-      .should('have.value', '2025-01-01')
+    cy.get('[data-cy="transaction-start-date"]').type('2026-05-01').should('have.value', '2026-05-01')
+    cy.get('[data-cy="transaction-end-date"]').type('2026-05-31').should('have.value', '2026-05-31')
+  })
 
-    cy.get('[data-cy="transaction-end-date"]')
-      .type('2025-12-31')
-      .should('have.value', '2025-12-31')
+  it('filtra por búsqueda de OXXO', () => {
+    cy.get('[data-cy="transaction-search-input"]').type('OXXO')
+    cy.get('[data-cy="transaction-card"]').should('have.length.greaterThan', 0)
+    cy.get('[data-cy="transaction-card"]').each(($card) => {
+      cy.wrap($card).should('contain.text', 'OXXO')
+    })
   })
 })
--- src/components/TransactionSearch.jsx
+++ src/components/TransactionSearch.jsx
@@ -5,6 +5,7 @@ function TransactionSearch({ searchTerm, setSearchTerm }) {
     <div className="banorte-search">
       <Search size={18} />
       <input
+        data-cy="transaction-search-input"
         type="text"
         placeholder="Buscar por comercio o monto"
         value={searchTerm}
